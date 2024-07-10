062b9e45fdd4d968cf0724076d3d24116fb36b6e
Nikola Simunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2024-07-10 09:54:41 +0200

    - controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,15 +22,33 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [numberOfItems, setnumberOfItems] = useState(1);
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    if (!description) return;
+    //console.log(event);
+    const newItem = {
+      description,
+      numberOfItems,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    initialItems.push(newItem);
+    setDescription("");
+    setnumberOfItems(0);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={numberOfItems}
+        onChange={(e) => setnumberOfItems(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((option) => (
           <option value={option} key={option}>
             {option}
@@ -38,7 +58,12 @@ function Form() {
         <option value={2}>2</option>
         <option value={3}>3</option> */}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
